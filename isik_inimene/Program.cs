bb1c457e0650d2a546e010c4bb4e4a41ef12ae14
opilane <opilane@HP-CZC6297B8F>
2022-09-09 09:09:10 +0300

    qq

--- isik_inimene/Program.cs
+++ isik_inimene/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using isik_inimene;
+
+Tootaja tootaja = new Tootaja("Aleksei", 2000, Tootaja.sugu.isane, "TTHK", "Tarkvara", 1600);
+Opilane opilane = new Opilane("Olesja", 2004, Opilane.sugu.emane, "Tallinna Kool", "LOGITpv20", ".");
+Kutsekooliopilane kutseop = new Kutsekooliopilane("Maksim", 1983, Opilane.sugu.isane, "TTHK", "Designer", 3);
+tootaja.print_Info();
+opilane.print_Info();
+kutseop.print_Info();
+
--- isik_inimene/Tootaja.cs
+++ isik_inimene/Tootaja.cs
@@ -3,18 +3,74 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static isik_inimene.Isik;
 
 namespace isik_inimene
 {
-    internal class Tootaja
+    internal class Tootaja : Isik
     {
-        protected string nimi;
-        protected int synniAasta;
-        protected char sugu;
 
-        public Tootaja(char sugu = 'x', string nimi = "---", int synniAasta = 0)
-        { this.nimi = nimi; this.synniAasta = synniAasta; this.sugu = sugu; }
-        public Tootaja(Isik ob)
-        { this.nimi = ob.nimi; this.synniAasta = ob.synniAasta; this.sugu = ob.sugu; }
+        public Tootaja(string nimi, int synniAasta, sugu Sugu, string asutus, string amet, double tootasu) : base(nimi, synniAasta, Sugu, asutus, amet, tootasu) { }
+
+        public Tootaja(Tootaja tootaja)
+        {
+            this.nimi = tootaja.nimi;
+            this.synniAasta = tootaja.synniAasta;
+            this.Sugu = tootaja.Sugu;
+            this.asutus = tootaja.asutus;
+            this.amet = tootaja.amet;
+            this.tootasu = tootaja.tootasu;
+        }
+        public override void print_Haal()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double arvutaSissetulek()
+        {
+            double result = tootasu;
+            result = tootasu - 500 + (tootasu * 0.2);
+
+            return result;
+        }
+
+        public override void print_Info()
+        {
+            Console.WriteLine($"Tootaja: \n---------\nNimi: {nimi} \nVanus: {arvutaVanus()} \nSugu: {Sugu} \nAsutus: {asutus} \nAmet: {amet} \nPalk: {randomSalary()} euro \nPreemia: {Premium()} euro", Console.ForegroundColor = ConsoleColor.White);
+            Console.WriteLine($"Tulumaks: {Tulumaks()} euro");
+        }
+
+        public override double arvutaSissetulek(double maksuvaba, double tulumaks)
+        {
+            throw new NotImplementedException();
+        }
+        public double randomSalary()
+        {
+            Random rand = new Random();
+            tootasu = rand.Next(1000, 2000);
+            tootasu += Premium();
+
+            return tootasu;
+        }
+        public double Tulumaks()
+        {
+            double tulumaks = Math.Round(tootasu - arvutaSissetulek(), 2);
+            return tulumaks;
+        }
+
+        public double Premium()
+        {
+            Random rand = new Random();
+            int premiumRand = rand.Next(1, 6);
+            double premium;
+            if (premiumRand > 0 && premiumRand < 3)
+            {
+                premium = 200;
+                return premium;
+            }
+            return 0;
+        }
+        public void muudaAasta(int uusAasta) { synniAasta = uusAasta; }
+        public void muudaSugu(sugu uusSugu) { Sugu = uusSugu; }
     }
 }
